81b295d98d76f1770e7fd55f67f7132e78b41bf0
Luiz Gustavo Torres <torresluizgtech@gmail.com>
2020-07-12 20:38:00 -0300

    building front-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
 const {
     connect
 } = require('../db/db.js');
 
-const PORT = 3000;
+const PORT = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
